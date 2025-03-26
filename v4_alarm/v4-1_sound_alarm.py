309b9bcfb3f76fcd8c7e779293d4900c676a50e6
murbachovski <126545394+murbachovski@users.noreply.github.com>
2025-03-27 08:50:08 +0900

    Delete v4_alarm directory

--- v4_alarm/v4-1_sound_alarm.py
+++
@@ -1,9 +0,0 @@
-# pip install playsound
-from playsound import playsound
-
-playsound('C:/Users/Administrator/Desktop/ai/wtdc/v4_alarm/input.mp3')
-
-print("WIN")
-
-# 알람 효과음 더미 사이트
-# https://pixabay.com/ko/sound-effects/search/%ea%b2%bd%ea%b3%a0%ec%9d%8c/
